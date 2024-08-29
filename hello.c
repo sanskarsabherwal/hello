f78704cb72fecce7dc11aa03b014010389f921d5
Sanskar Sabherwal <ssabherwal2@huskers.unl.edu>
2024-08-29 17:08:59 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: NAME
+ *         EMAIL
+ * Date: 20xx/xx/xx
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!");
+
+  return 0;
+}
